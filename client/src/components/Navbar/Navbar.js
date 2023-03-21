13df36b9b0759ac76e540da2a73c552f0ae9d895
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-21 10:44:23 +0100

    dynamic navbar depending on authentication

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,20 +1,30 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { useContext } from 'react';
+import { authContext } from '../../contexts/autContext';
 
 const Navbar = () => {
+
+    const { isAuthenticated, userEmail } = useContext(authContext);
+
     return (
         <nav className={styles['navbar']}>
             <ul>
                 <li><Link to="/">Dashboard</Link></li>
-                <li><Link to="/employees">Employees</Link></li>
-                <li><Link to="/tasks">Tasks</Link></li>
-
-                {/* for authenticated users */}
-                <li><Link to="/auth/logout">Logout</Link></li>
-                {/* for not authenticated users */}
-                <li><Link to="/auth/login">Login</Link></li>
-                <li><Link to="/auth/register">Register</Link></li>
-
+                {isAuthenticated && (
+                    <div className='user'>
+                    <li>Hello {userEmail}</li>
+                    <li><Link to="/employees">Employees</Link></li>
+                    <li><Link to="/tasks">Tasks</Link></li>
+                    <li><Link to="/auth/logout">Logout</Link></li>
+                    </div>
+                )}
+                {!isAuthenticated && (
+                    <div className='guest'>
+                    <li><Link to="/auth/login">Login</Link></li>
+                    <li><Link to="/auth/register">Register</Link></li>
+                    </div>
+                )}
             </ul>
         </nav>
     );
