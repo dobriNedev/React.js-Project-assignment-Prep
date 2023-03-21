e5e98db55f490f906e59ef360bf9e915e3534b6f
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-21 10:44:52 +0100

    modified

--- client/src/utils/requester.js
+++ client/src/utils/requester.js
@@ -1,27 +1,30 @@
 const request = async (method, url, data) => {
     const options = {};
-    
+
     if (method !== 'GET') {
         options.method = method;
 
         if (data) {
             options.headers = {
-                'content-type': 'application/json',
-            };
-
+                'Content-Type': 'application/json'
+            }
             options.body = JSON.stringify(data);
         }
     }
 
     const response = await fetch(url, options);
 
-    try {
-        const result = await response.json();
-
-        return result;
-    } catch (error) {
+    if (response.status === 204) {
         return {};
     }
+
+    const result = await response.json();
+
+    if(!response.ok) {    
+        throw result;
+    }
+
+    return result;
 };
 
 export const get = request.bind(null, 'GET');
