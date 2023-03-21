eccbd205b57926694cf570f667b90df13029f7d0
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-21 11:22:57 +0100

    register works

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,21 +1,71 @@
+import useForm from "../../hooks/useForm";
+import { authContext } from "../../contexts/autContext";
+import { useContext } from "react";
+
 const Register = () => {
+
+    const { onRegisterSubmit } = useContext(authContext);
+
+    const { values, onChangeHandler, onSubmit } = useForm({
+        firstName: '', 
+        lastName: '',
+        email: '',
+        password: '',
+        rePass: ''
+    }, onRegisterSubmit);
+
     return(
-        <form method="POST">
+        <form method="POST" onSubmit={onSubmit}>
             <h1>Register</h1>
             <label forhtml="firstName">Firts name:</label>
-            <input type="text" id="firstName" name="firstName" required/>
+            <input 
+                type="text" 
+                id="firstName" 
+                name="firstName" 
+                required
+                onChange={onChangeHandler}
+                value={values.firstName}
+                />
             <br></br>
             <label forhtml="lastName">Last name:</label>
-            <input type="text" id="lastName" name="lastName" required/>
+            <input 
+                type="text" 
+                id="lastName" 
+                name="lastName" 
+                required
+                onChange={onChangeHandler}
+                value={values.lastName}
+                />
             <br></br>
             <label forhtml="email">Email:</label>
-            <input type="email" id="email" name="email" required/>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                required
+                onChange={onChangeHandler}
+                value={values.email}
+                />
             <br></br>
             <label forhtml="password">Password:</label>
-            <input type="password" id="password" name="password" required/>
+            <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                required
+                onChange={onChangeHandler}
+                value={values.password}
+                />
             <br></br>
             <label forhtml="rePass">Confirm Password:</label>
-            <input type="password" id="rePass" name="rePass" required/>
+            <input 
+                type="password" 
+                id="rePass" 
+                name="rePass" 
+                required
+                onChange={onChangeHandler}
+                value={values.rePass}
+                />
             <br></br>
             <button type="submit">Register</button>
         </form>
