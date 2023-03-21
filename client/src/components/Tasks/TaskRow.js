ce03cc8a12ed5ecf67a506028349b0803aeb71b3
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-21 22:54:59 +0100

    dynamic buttons based on ownership

--- client/src/components/Tasks/TaskRow.js
+++ client/src/components/Tasks/TaskRow.js
@@ -1,36 +1,48 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { authContext } from "../../contexts/autContext";
 
 const TaskRow = ({
-  _id,
-  title,
-  description,
-  assignee,
-  dueDate,
-  status,
-
+    _id,
+    title,
+    description,
+    assignee,
+    dueDate,
+    status,
+    _ownerId
 }) => {
-    return(
+    const { userId } = useContext(authContext);
+
+    const isOwner = _ownerId === userId;
+
+    return (
         <tr>
-  <td>{title}</td>
-  <td>{description}</td>
-  <td>
-  {/* {{#each this.assignee}}
+            <td>{title}</td>
+            <td>{description}</td>
+            <td>
+                {/* {{#each this.assignee}}
     {{this.firstName}} {{this.lastName}}
     {{#unless @last}}, {{/unless}}
   {{/each}} */}
-  {assignee}
-  </td>
-  <td>{dueDate}</td>
-  <td>{status}</td>
-  <td>
-    {/* {{#if (eq this.status 'closed')}}
+                {assignee}
+            </td>
+            <td>{dueDate}</td>
+            <td>{status}</td>
+
+            {/* {{#if (eq this.status 'closed')}}
     {{else}}
       {{#if (eq this.status 'open')}} */}
-      <Link className="action-edit" to={`/tasks/${_id}/edit`}>Edit</Link>
-      <Link className="action-delete" to={`/tasks/${_id}/delete`}>Delete</Link>
-      <Link className="action-assign" to={`/tasks/${_id}/assign`}>Assign</Link>
-  </td>
-</tr>
+            {/* TODO: add check for status too - only opened tasks can be deleted or/and edited */}
+            {isOwner && (
+                <td>
+                    <Link className="action-edit" to={`/tasks/${_id}/edit`}>Edit</Link>
+                    <Link className="action-delete" to={`/tasks/${_id}/delete`}>Delete</Link>
+                </td>
+            )}
+            <td>
+                <Link className="action-assign" to={`/tasks/${_id}/assign`}>Assign</Link>
+            </td>
+        </tr>
     );
 };
 
