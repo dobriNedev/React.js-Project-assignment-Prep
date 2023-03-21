a0c5586c350ba8fb3b4442bd7153233f38611e7e
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-21 10:44:37 +0100

    authService added

---
+++ client/src/services/authService.js
@@ -0,0 +1,7 @@
+import * as request from '../utils/requester';
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const register = (data) => request.post(`${baseUrl}/register`, data);
+
+export const login = (data) => request.post(`${baseUrl}/login`, data);
