760e748807ca736c4ea5ec64ab70b70d3e507797
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-06 14:24:19 +0200

    bootstraped

--- client/src/components/Employees/EmployeeRow.js
+++ client/src/components/Employees/EmployeeRow.js
@@ -1,56 +1,18 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { authContext } from "../../contexts/autContext";
-import { useService } from "../../hooks/useService";
-import { employeeServiceFactory } from "../../services/empolyeeService";
+import { Link } from "react-router-dom";
+import EmployeeDetails from "./EmployeeDetails";
 
-const EmployeeRow = ({
-    _id,
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    birthDate,
-    salary,
-    role,
-    _ownerId
-}) => {
-    const navigate = useNavigate();
-
-    const { userId } = useContext(authContext);
-
-    const isOwner = _ownerId === userId;
-
-    const employeeService = useService(employeeServiceFactory);
-
-    const onDeleteClick = async() => {
-        await employeeService.delete(_id);
-
-        //TODO: change employees state
-
-        navigate('/employees');
-    }
-
-    return(
-        <tr>
-    <td>{firstName} {lastName}</td>
-    <td>{email}</td>
-    <td>{phoneNumber}</td>
-    <td>{birthDate}</td>
-    <td>{salary}</td>
-    <td>{role}</td>
-    {isOwner && (
-            <td>
-            <Link className="action-edit" to={`/employees/${_id}/edit`}> Edit </Link>
-            {/* <Link className="action-delete" to={`/employees/${_id}/delete`}>Delete</Link> */}
-            <button onClick={onDeleteClick}>Delete</button>
-        </td>
-    )}
-    <td>
-        <Link className="action-assign" to={`/employees/${_id}/tasks`}>Tasks</Link>
-    </td>
-</tr>
-    );
+const EmployeeRow = (employee) => {
+  return (
+    <tr>
+      <td>{employee.firstName}</td>
+      <td>{employee.lastName}</td>
+      <td>{employee.role}</td>
+      <td>
+        {/* <Link className="action-edit" to={`/employees/${_id}/details`}></Link> */}
+        <EmployeeDetails {...employee} />
+      </td>
+    </tr>
+  );
 };
 
 export default EmployeeRow;
--- client/src/components/Employees/Employees.js
+++ client/src/components/Employees/Employees.js
@@ -1,43 +1,47 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { employeeContex } from '../../contexts/employeeContext';
-import EmployeeRow from './EmployeeRow';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { employeeContex } from "../../contexts/employeeContext";
+import EmployeeRow from "./EmployeeRow";
 
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Employees = () => {
-    const { employees } = useContext(employeeContex);
-    return(
-        <section className="list">
-        <h2 className="section-title">Employees</h2>
-        {employees.length !== 0 && (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Date of Birth</th>
-                        <th>Monthly Salary</th>
-                        <th>Role</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(e => <EmployeeRow key={e._id} {...e}/> )}
-                </tbody>
-            </table>
-        )}
-             
-        {employees.length === 0 && (
-            <div>
-            <h2>No employees yet!</h2>
-            </div>
-        )}
-         <div className="add">
-            <Link className="action-edit" to="/employees/create">Add new employee</Link>
+  const { employees } = useContext(employeeContex);
+  return (
+    <section className="list">
+      <h2 className="section-title">Employees</h2>
+      {employees.length === 0 && (
+        <div>
+          <h2>No employees yet!</h2>
         </div>
+      )}
+      {employees.length !== 0 && (
+        <>
+          <Table striped>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((e) => (
+                <EmployeeRow key={e._id} {...e} />
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+      <div className="add">
+        <Link className="action-edit" to="/employees/create">
+          Add new employee
+        </Link>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Employees;
