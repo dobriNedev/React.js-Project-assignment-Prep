aeddf1a51dcc12318de9f6eb4dd02c0ce68bb950
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-11 18:18:46 +0200

    Paginatio component added

---
+++ client/src/components/Pagination/MyPagination.js
@@ -0,0 +1,25 @@
+import Pagination from "react-bootstrap/Pagination";
+
+const MyPagination = ({ currentPage, totalPages, onPageChange }) => {
+  let active = currentPage;
+  let items = [];
+  for (let number = 1; number <= totalPages; number++) {
+    items.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => onPageChange(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  return (
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
+  );
+};
+
+export default MyPagination;
