02e974f8d928ebebb770c0a71f928e6cfd03bf38
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-17 21:45:48 +0200

    Login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,107 +1,74 @@
-import { Link } from "react-router-dom";
-import useForm from "../../hooks/useForm";
-import { useContext } from "react";
-import { authContext } from "../../contexts/autContext";
+import styles from "./Login.module.css";
 
-const Login = () => {
-  const { onLoginSubmit } = useContext(authContext);
+import { useState } from "react";
 
-  const { values, onChangeHandler, onSubmit } = useForm(
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
+import { useValidation } from "../../hooks/useValidation";
+
+export const Login = () => {
+  const [hasError, setHasError] = useState(false);
+
+  const onErrorSubmit = () => {
+    setHasError(true);
+  };
+
+  const { onLoginSubmit } = useAuthContext();
+  const { values, changeHandler, onSubmit } = useForm(
     {
       email: "",
       password: "",
     },
-    onLoginSubmit
+    (values) => {
+      onLoginSubmit(values, onErrorSubmit);
+    }
   );
+
+  const { validateEmail, isValid, onBlurHandler } = useValidation();
 
   return (
-    <form method="POST" onSubmit={onSubmit}>
-      <h2>Login to start managing tasks and employees</h2>
-      <br></br>
-      <label forhtml="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        required
-        onChange={onChangeHandler}
-        value={values.email}
-      />
-      <br></br>
-      <label forhtml="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        required
-        onChange={onChangeHandler}
-        value={values.password}
-      />
-      <br></br>
-      <button type="submit">Login</button>
-      <div className="down-div">
-        <p>
-          Don't have an account yet? Just{" "}
-          <Link to="/auth/register">register</Link> please!
-        </p>
+    <section className={styles.login}>
+      <div className={styles.container}>
+        <form onSubmit={onSubmit}>
+          <h2>Login</h2>
+          {hasError && (
+            <div className={styles.loginError}>
+              Invalid username or password
+            </div>
+          )}
+          {!isValid && (
+            <div className={styles.emailValidation}>Email is not valid</div>
+          )}
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={(e) => {
+              changeHandler(e);
+              validateEmail(e.target.value);
+              setHasError(false);
+            }}
+            onBlur={(e) => onBlurHandler(e.target.value)}
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={(e) => {
+              changeHandler(e);
+              setHasError(false);
+            }}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
       </div>
-    </form>
+    </section>
   );
 };
-
-export default Login;
-
-// import useForm from "../../hooks/useForm";
-// import { useContext } from "react";
-// import { authContext } from "../../contexts/autContext";
-
-// import Button from "react-bootstrap/Button";
-// import Form from "react-bootstrap/Form";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// const Login = () => {
-//   const { onLoginSubmit } = useContext(authContext);
-
-//   const { values, onChangeHandler, onSubmit } = useForm(
-//     {
-//       email: "",
-//       password: "",
-//     },
-//     onLoginSubmit
-//   );
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <Form onSubmit={onSubmit}>
-//         <Form.Group className="mb-3" controlId="formBasicEmail">
-//           <Form.Label>Email address</Form.Label>
-//           <Form.Control
-//             type="email"
-//             placeholder="Enter email"
-//             onChange={onChangeHandler}
-//             value={values.email}
-//           />
-//           <Form.Text className="text-muted">
-//             We'll never share your email with anyone else.
-//           </Form.Text>
-//         </Form.Group>
-
-//         <Form.Group className="mb-3" controlId="formBasicPassword">
-//           <Form.Label>Password</Form.Label>
-//           <Form.Control
-//             type="password"
-//             placeholder="Password"
-//             onChange={onChangeHandler}
-//             value={values.password}
-//           />
-//         </Form.Group>
-
-//         <Button variant="primary" type="submit">
-//           Login
-//         </Button>
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default Login;
