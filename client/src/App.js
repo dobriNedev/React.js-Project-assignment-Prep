4166232a70a4bc53f9890d719046046ac1076915
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-17 21:27:23 +0200

    App modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,56 @@
-import { AuthProvider } from "./contexts/autContext";
-import { EmployeeProvider } from "./contexts/employeeContext";
-import { TaskProvider } from "./contexts/taskContext";
+import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import { Routing } from "./components/Routing/Routing";
-import { EmployeeTasksProvider } from "./contexts/EmployeeTasksContext";
+import { AuthProviderComponent } from "./contexts/AuthContext";
+import { CarProvider } from "./contexts/CarContext";
+
+import { CreateCar } from "./components/CreateCar/CreateCar";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Catalog } from "./components/Catalog/Catalog";
+import { Register } from "./components/Register/Register";
+import { CarDetails } from "./components/CarDetails/CarDetails";
+import { Logout } from "./components/Logout/Logout";
+import { EditCar } from "./components/EditCar/EditCar";
+import { RouteGuard } from "./common/RouteCuard";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
+
+import styles from "./App.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Header />
-        <Navbar />
-        <main className="main">
-          <EmployeeProvider>
-            <TaskProvider>
-              <EmployeeTasksProvider>
-                <Routing />
-              </EmployeeTasksProvider>
-            </TaskProvider>
-          </EmployeeProvider>
-        </main>
-        <Footer />
-      </div>
-    </AuthProvider>
+    <AuthProviderComponent>
+      <CarProvider>
+        <div className="allContent">
+          <Header />
+          <main className={styles.main}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/logout" element={<Logout />} />
+
+              <Route element={<RouteGuard />}>
+                <Route path="/create" element={<CreateCar />} />
+              </Route>
+
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/catalog/:carId" element={<CarDetails />} />
+
+              <Route element={<RouteGuard />}>
+                <Route path="/catalog/:carId/edit" element={<EditCar />} />
+              </Route>
+
+              <Route path="/404" element={<PageNotFound />}></Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </CarProvider>
+    </AuthProviderComponent>
   );
 }
 
