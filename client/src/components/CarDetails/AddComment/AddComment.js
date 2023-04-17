7d00ca2dd1dea84fe4051333d2c11a31ca8bc33b
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-17 21:52:22 +0200

    AddComment componenet

---
+++ client/src/components/CarDetails/AddComment/AddComment.js
@@ -0,0 +1,26 @@
+import styles from "./AddComment.module.css";
+import { useForm } from "../../../hooks/useForm";
+
+export const AddComment = ({ onCommentSubmit }) => {
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      comment: "",
+    },
+    onCommentSubmit
+  );
+
+  return (
+    <form method="POST" onSubmit={onSubmit} className={styles["comment-form"]}>
+      <label>
+        Add a comment:
+        <textarea
+          name="comment"
+          placeholder="Comment......"
+          value={values.comment}
+          onChange={changeHandler}
+        ></textarea>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
