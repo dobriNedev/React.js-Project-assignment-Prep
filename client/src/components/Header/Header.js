f5d8b684d2168c223850b0ed1d77edaf94d49f8f
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-17 21:36:51 +0200

    Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,48 @@
-const Header = () => {
-    return (
-        <header>
-            <h1>Employee Management System</h1>
-        </header>
-    );
-};
+import { Link } from "react-router-dom";
 
-export default Header;
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
+
+import styles from "./Header.module.css";
+
+export const Header = () => {
+  const { isAuthenticated, userEmail } = useContext(AuthContext);
+
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {isAuthenticated && <li>Welcome {userEmail}</li>}
+          <li>
+            <Link to="/catalog">Cars</Link>
+          </li>
+
+          {isAuthenticated && (
+            <>
+              <li>
+                <Link to="/create">Add Car</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </>
+          )}
+
+          {!isAuthenticated && (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
